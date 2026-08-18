ed77313a052a46edd2c8f08c4a25145f0ca3f852
NG <nils.geissler@uni-koeln.de>
2026-08-18 11:40:27 +0200

    added debug option to test compare forwarded client ip

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from app.config import settings
 from app.routers.records import router as records_router
 
@@ -15,3 +15,12 @@ async def startup():
     # Intentionally empty:
     # indexes are created during the import process only
     pass
+
+@app.get("/debug/client-ip")
+async def debug_client_ip(request: Request):
+    return {
+        "client_host": request.client.host if request.client else None,
+        "x_forwarded_for": request.headers.get("x-forwarded-for"),
+        "x_real_ip": request.headers.get("x-real-ip"),
+        "forwarded": request.headers.get("forwarded"),
+    }
